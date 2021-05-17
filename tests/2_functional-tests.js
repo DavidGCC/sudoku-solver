b7fa65fd020b005975a90236ea4ed69201edcfa7
marshallg <datog309@gmail.com>
2021-05-17 12:26:24 +0400

    D:/Git/api/solve tests done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,28 +5,71 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
+const ps = "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+const solvedPs = "135762984946381257728459613694517832812936745357824196473298561581673429269145378";
+
 suite('Functional Tests', () => {
 
     suite("/api/solve POST request tests", () => {
 
-        test("Solve a puzzle with valid puzzle string: POST request to /api/solve", () => {
-
+        test("Solve a puzzle with valid puzzle string: POST request to /api/solve", (done) => {
+            chai.request(server)
+                .post("/api/solve")
+                .set("Content-Type", "application/json")
+                .send({ puzzle: ps })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.deepEqual(res.body, { result: solvedPs });
+                    assert.equal(res.body.result.includes("."), false);
+                    assert.equal(/^[1-9]{81}$/.test(res.body.result), true);
+                    done();
+                });
         });
 
-        test("Solve a puzzle with missing puzzle string: POST request to /api/solve", () => {
-
+        test("Solve a puzzle with missing puzzle string: POST request to /api/solve", (done) => {
+            chai.request(server)
+                .post("/api/solve")
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.deepEqual(res.body, { error: "Required field missing" });
+                    done();
+                });
         });
 
-        test("Solve a puzzle with invalid characters: POST request to /api/solve", () => {
-
+        test("Solve a puzzle with invalid characters: POST request to /api/solve", (done) => {
+            chai.request(server)
+                .post("/api/solve")
+                .set("Content-Type", "application/json")
+                .send({ puzzle: "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.s" })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.deepEqual(res.body, { error: "Invalid characters in puzzle" });
+                    done();
+                });
         });
 
-        test("Solve a puzzle with incorrect length: POST request to /api/solve", () => {
-
+        test("Solve a puzzle with incorrect length: POST request to /api/solve", (done) => {
+            chai.request(server)
+                .post("/api/solve")
+                .set("Content-Type", "application/json")
+                .send({ puzzle: "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6." })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.deepEqual(res.body, { error: "Expected puzzle to be 81 characters long" });
+                    done();
+                });
         });
 
-        test("Solve a puzzle that cannot be solved: POST request to /api/solve", () => {
-
+        test("Solve a puzzle that cannot be solved: POST request to /api/solve", (done) => {
+            chai.request(server)
+                .post("/api/solve")
+                .set("Content-Type", "application/json")
+                .send({ puzzle: "...7...92.........2....95..57.3...46.........1...2......2.9.....4....2....8.7...." })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.deepEqual(res.body, { error: "Puzzle cannot be solved" });
+                    done();
+                })
         });
 
     });
