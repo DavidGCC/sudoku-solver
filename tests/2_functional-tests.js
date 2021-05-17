fcdfb05f0dbd01499d7fcff0634e19f1da85a408
marshallg <datog309@gmail.com>
2021-05-17 13:13:23 +0400

    D:/Git/api/check tests done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -75,45 +75,113 @@ suite('Functional Tests', () => {
     });
 
     suite("/api/check POST request tests", () => {
-        
-        test("Solve a puzzle with valid puzzle string: POST request to /api/solve", () => {
 
-        });
-
-        test("Check a puzzle placement with all fields: POST request to /api/check", () => {
-
+        test("Check a puzzle placement with all fields: POST request to /api/check", (done) => {
+            chai.request(server)
+                .post("/api/check")
+                .set("Content-Type", "applciation/json")
+                .send({ puzzle: ps, coordinate: "A2",  value: 6})
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.deepEqual(res.body, { valid: true });
+                    done();
+                });
         });
 
         test("Check a puzzle placement with single placement conflict: POST request to /api/check", () => {
-
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: ps, coordinate: "A2",  value: 1})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { valid: false, "conflict": [ "row" ] });
+                done();
+            });
         });
 
         test("Check a puzzle placement with multiple placement conflicts: POST request to /api/check", () => {
-
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: ps, coordinate: "A2",  value: 2})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { valid: false, conflict: [ "column", "region" ] });
+                done();
+            });
         });
 
-        test("Check a puzzle placement with multiple placement conflicts: POST request to /api/check", () => {
-
+        test("Check a puzzle placement with all placement conflicts: POST request to /api/check", () => {
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: ps, coordinate: "A2",  value: 5})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { valid: false, conflict: [ "row", "column", "region" ] });
+                done();
+            });
         });
 
         test("Check a puzzle placement with missing required fields: POST request to /api/check", () => {
-
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: ps, coordinate: "A2"})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { error: "Required field(s) missing" });
+                done();
+            });
         });
 
         test("Check a puzzle placement with invalid characters: POST request to /api/check", () => {
-
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.s", coordinate: "A2",  value: 6})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { error: "Invalid characters in puzzle" });
+                done();
+            });
         });
 
         test("Check a puzzle placement with incorrect length: POST request to /api/check", () => {
-
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6", coordinate: "A2",  value: 6})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { error: "Expected puzzle to be 81 characters long" });
+                done();
+            });
         });
         
-        test("Check a puzzle placement with incorrect length: POST request to /api/check", () => {
-
+        test("Check a puzzle placement with invalid placement coordinate: POST request to /api/check", () => {
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: ps, coordinate: "S2",  value: 6})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { error: "Invalid coordinate" });
+                done();
+            });
         });
 
         test("Check a puzzle placement with invalid placement value: POST request to /api/check", () => {
-
+            chai.request(server)
+            .post("/api/check")
+            .set("Content-Type", "applciation/json")
+            .send({ puzzle: ps, coordinate: "A2",  value: 0})
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { error: "Invalid value" });
+                done();
+            });
         });
 
     });
